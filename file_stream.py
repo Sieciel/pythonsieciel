6d3794ecf21ff5d65cef181828c2842d2fdc6803
sieciel <luk.ciesla@gmail.com>
2024-03-19 15:07:57 +0100

    update

--- file_stream.py
+++ file_stream.py
@@ -15,3 +15,36 @@ default            192.168.1.1        UGSc       45        0     en0
 10.0.0.0/24        link#2             UCSc        2        0     en1
 172.16.0.0/16      172.16.0.1         UGSc        0        0     en2
 192.168.1.0/24     link#6             UCSc       10        0     en0"""
+
+#1
+
+with open("ip_addresses.txt", "w") as file:
+    file.write(ip_addresses_content)
+
+with open("routing_table.txt", "w") as file:
+    file.write(routing_table_content)
+
+"ip_addresses.txt", "routing_table.txt"
+
+#2
+# with open('ip_addresses.txt', 'r') as file:
+#     contents = file.read()
+#     print(contents)
+
+#3
+with open('routing_table.txt' , 'r') as file:
+    for line in file:
+        print(line.strip())
+
+#4
+subnet_masks = ['255.255.255.0' , '255.255.0.0' , '255.0.0.0']
+with open('subnet_masks.txt' , 'w') as file:
+    for mask in subnet_masks:
+        file.write(mask + '\n')
+
+#5
+target_network = '192.168.1.0/24'
+with open('routing_table.txt' , 'r') as file:
+    for line in file:
+        if target_network in line:
+            print(line.strip())
