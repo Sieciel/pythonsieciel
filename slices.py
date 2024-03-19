4385c89a0d2e8e569dceb90d9a62e26f8319b028
sieciel <luk.ciesla@gmail.com>
2024-03-19 14:30:33 +0100

    stringi upd

--- slices.py
+++ slices.py
@@ -1,2 +1,16 @@
 #---Slices (str.47)
 
+#1
+greeting = "Hello, Python!"
+sub_greeting = greeting[0:5]
+print(sub_greeting)
+
+language = greeting[7:]
+print(language)
+
+#2
+skip_chars = greeting[::2]
+print(skip_chars)
+
+reversed_greeting = greeting[::-1]
+print(reversed_greeting)
