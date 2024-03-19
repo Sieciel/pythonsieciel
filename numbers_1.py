d234c0a332cda7342e9c68a533ad78e7f9daca2d
sieciel <luk.ciesla@gmail.com>
2024-03-19 14:42:51 +0100

    number commit

--- numbers_1.py
+++ numbers_1.py
@@ -13,5 +13,18 @@ b = 0.5
 product = a * b
 print(f"Product: {product}")
 
+#3
+result = 10 // 3
+print(f"Integer Division Result: {result}")
+
+float_result= 10.0 // 3
+print(f"Float result from Integer Deivision: {float_result}")
 
 
+float_value = 3.14
+int_value = int(float_value)
+print(f"Converted Integer: {int_value}")
+
+int_number = 5
+float_number = float(int_number)
+print(f"Converted Float: {float_number}")
