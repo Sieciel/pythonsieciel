4c7ad4920515524b589a8d2a29c6f767a9bebdf7
sieciel <luk.ciesla@gmail.com>
2024-03-20 10:37:10 +0100

    comp_upd2

--- comprehensions.py
+++ comprehensions.py
@@ -9,3 +9,17 @@ print(squares)
 evens = [n for n in numbers if n %2 == 0]
 print(evens)
 
+#3
+ip_list = ["192.168.1.1" , "10.0.0.256" , "172.16.0.1" , "256.0.0.1"]
+# valid_ips = [ip for ip in ip_list if 0<=int(ip.split('.')[0])<=255] #sprawdza tylko pierwszy oktet
+valid_ips = [ip for ip in ip_list if all (0 <=int(octet) <= 255 for octet in ip.split("."))]  #sprawdza wszystkie 4 oktety w adresie IP
+print(valid_ips)
+
+#4
+interfaces = [f"GigabitEthernet0/{i} "for i in range(0,5)]
+print(interfaces)
+
+#5
+subnets = [["192.168.1.1", "192.168.1.2"], ["10.0.0.1", "10.0.0.2"]]
+all_ips = [ip for subnet in subnets for ip in subnet]
+print(all_ips)
