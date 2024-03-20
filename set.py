07f462ae754645cb9b98355bc294d89241688f32
sieciel <luk.ciesla@gmail.com>
2024-03-20 11:42:42 +0100

    set_update2

--- set.py
+++ set.py
@@ -1,2 +1,35 @@
 #---Set
 
+#1
+vlans = {10,20,30,40,50}
+protocols = set(['OSPF', 'BGP', 'EIGRP'])
+print(vlans)
+print(protocols)
+
+#2
+vlans.add(60)
+print(vlans)
+
+# #3
+# vlans.remove(10)
+# print(vlans)
+
+#4
+vlans.remove(10)
+protocols.discard('EIGRP')
+print(vlans)
+print(protocols)
+
+#5
+a = {1,2,3}
+b = {3,4,5}
+union_set = a.union(b)
+print(union_set)
+
+#6
+intersection_set = a.intersection(b)
+print(intersection_set)
+
+#7
+difference_set = a.difference(b)
+print(difference_set)
