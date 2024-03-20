c5e0628e167d4df93419b74afed3ba15bfeff44e
sieciel <luk.ciesla@gmail.com>
2024-03-20 14:43:04 +0100

    yaml_upd3

--- dupajaml_1.py
+++ dupajaml_1.py
@@ -1,5 +1,5 @@
 #--- YAML
-import json
+#import json
 # #1
 # import yaml
 # with open('network_config.yaml', 'r') as file:
@@ -7,9 +7,29 @@ import json
 # print(network_config)
 
 #2
-network_data = {
-    'switch': 'Switch1',
-    'ports': [{'name':'FastEthernet0/1', 'vlan':'10'}, {'name': 'FastEthernet0/2', 'vlan':'20'}]
-}
-with open('new_network_config.json', 'w') as file:
-  json.dump(network_data, file, indent=4)
+# network_data = {
+#     'switch': 'Switch1',
+#     'ports': [{'name':'FastEthernet0/1', 'vlan':'10'}, {'name': 'FastEthernet0/2', 'vlan':'20'}]
+# }
+# with open('new_network_config.json', 'w') as file:
+#   json.dump(network_data, file, indent=4)
+
+  #3
+  import yaml
+
+  def read_and_modify_router_config(file_path):
+    with open(file_path, 'r') as file:
+      try:
+        data = yaml.safe_load(file)
+        if data['interfaces']:
+          data['interfaces'][0]['ip'] = '10.10.10.1'
+        return data
+      except yamlYAMLError as exc:
+        print(exc)
+
+    def write_yaml(file_path, data):
+      with open(file_path, 'w') as file:
+        yaml.dump(data, file, sort_keys=False)
+
+    modified_config = read_and_modify_router_config('network_config.yml')
+    write_yaml('modified_router_config.yml', modified_config)
