3ea9a8e25d2b602135b643342cde383b61ee1406
sieciel <luk.ciesla@gmail.com>
2024-03-20 14:32:32 +0100

    dsds

--- dupajaml_1.py
+++ dupajaml_1.py
@@ -5,3 +5,11 @@ import yaml
 with open('network_config.yaml', 'r') as file:
     network_config = yaml.safe_load(file)
 print(network_config)
+
+#2
+network_data = {
+    'switch': 'Switch1',
+    'ports': [{"name":'FastEthernet0/1', 'vlan':'10'}, {'name': 'FastEthernet0/2', 'vlan':'20'}]
+}
+with open('new_network_config.json', 'w') as file:
+  json.dump(network_data, file, indent=4)
